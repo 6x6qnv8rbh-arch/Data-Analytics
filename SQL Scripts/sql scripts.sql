fce58016667145b8025c58559b9b1224b33b4c24
Keerthi HN <6x6qnv8rbh@privaterelay.appleid.com>
2025-12-18 12:30:10 +0530

    Add files via upload

---
+++ SQL Scripts/sql scripts.sql
@@ -0,0 +1,8 @@
+create database colleges;
+create database marks;
+use colleges;
+create table students(rollno int primary key,name varchar(20),age int);
+select * from students;
+insert into students values(101,"keerthi",22);
+insert into students values(102,"lekhu",20);
+insert into students values(103,"hari",23);
